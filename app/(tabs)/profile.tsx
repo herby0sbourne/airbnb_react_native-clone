51a3d99e194f707d0eff5e638a3587e0e811fc23
Herby <herbyosbourne31@gmail.com>
2024-05-16 08:27:21 -0500

    profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from 'react-native';
+import { useAuth } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
+import { View, Text, Button, StyleSheet } from 'react-native';
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
+
+  console.log(isSignedIn);
+
   return (
     <View>
-      <Text>profile</Text>
+      <Button title="Log out" onPress={() => signOut} />
+      {!isSignedIn && (
+        <Link href={'/(modals)/login'}>
+          <Text>Login</Text>
+        </Link>
+      )}
     </View>
   );
 };
+
 export default Page;

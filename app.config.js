5dd599fea7e1e90bd6f50b035826ae3d80cbda7b
Herby <herbyosbourne31@gmail.com>
2024-05-21 09:12:36 -0500

    remove unused code

--- app.config.js
+++ app.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   name: 'Airbnb',
-  version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
+  // version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
   scheme: 'myapp',
   extra: {
-    fact: 'kittens are cool',
     clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
 };

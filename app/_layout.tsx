5b06acc9616be937a7087d92522c1b3a63994d5a
Herby <herbyosbourne31@gmail.com>
2024-05-21 07:45:53 -0500

    update error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,14 +13,14 @@ const tokenCache = {
   async getToken(key: string) {
     try {
       return SecureStore.getItemAsync(key);
-    } catch (error) {
+    } catch (err) {
       return null;
     }
   },
   async saveToken(key: string, value: string) {
     try {
       return SecureStore.setItemAsync(key, value);
-    } catch (error) {
+    } catch (err) {
       return;
     }
   },

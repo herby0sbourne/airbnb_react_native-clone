432f429be4aeb04274f4ef9f3a46fe6784405259
Herby <herbyosbourne31@gmail.com>
2024-05-23 18:48:42 -0500

    fix signout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,8 @@
 import { useAuth } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button } from 'react-native';
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
-
-  console.log(isSignedIn);
 
   return (
     <View>

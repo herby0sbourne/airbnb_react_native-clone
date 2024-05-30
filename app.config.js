f322f2665520b99ec0fcfdf35c262de3a31705e1
Herby <herbyosbourne31@gmail.com>
2024-05-30 10:28:48 -0500

    update config

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'Airbnb',
+  // name: "Airbnb",
   // version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
-  scheme: 'myapp',
+  scheme: "myapp",
   extra: {
     clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },

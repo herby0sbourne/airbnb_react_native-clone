a7878cde7da1d95c8b9e5bb2a431d47724e5145e
Herby <herbyosbourne31@gmail.com>
2024-05-30 10:28:35 -0500

    refactor import

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,17 @@
-import { Stack } from 'expo-router';
-import { View } from 'react-native';
-import { useMemo, useState } from 'react';
+import { Stack } from "expo-router";
+import { View } from "react-native";
+import { useMemo, useState } from "react";
 
-import Listings from '@/components/Listings';
-import ExploreHeader from '@/components/ExploreHeader';
+import Listings from "@/components/Listings";
+import ListingsMap from "@/components/ListingsMap";
+import ExploreHeader from "@/components/ExploreHeader";
 
-import { Listing } from '@/types';
-import listingsData from '@/assets/data/air-bnb-listings.json';
+import { Listing } from "@/types";
+import listingsData from "@/assets/data/air-bnb-listings.json";
+// import listingsDataGeo from "@/assets/data/airbnb-listings.geo.json";
 
 const Page = () => {
-  const [category, setCategory] = useState('Tiny Home');
+  const [category, setCategory] = useState("Tiny Home");
   const items = useMemo(() => listingsData as Listing[], []);
 
   const handleCategoryChange = (category: string) => {
@@ -20,10 +22,13 @@ const Page = () => {
     <View style={{ flex: 1 }}>
       <Stack.Screen
         options={{
-          header: () => <ExploreHeader onCategoryChange={handleCategoryChange} />,
+          header: () => (
+            <ExploreHeader onCategoryChange={handleCategoryChange} />
+          ),
         }}
       />
-      <Listings category={category} listings={items.toSpliced(2)} />
+      {/*<Listings category={category} listings={items.toSpliced(2)} />*/}
+      <ListingsMap listings={items as Listing[]} />
     </View>
   );
 };

cf120cd2e1573e5f0e74b9a50579ccab4d398ac3
Herby <herbyosbourne31@gmail.com>
2024-06-01 16:49:29 -0500

    add button the show map on press

--- components/ListingsBottomSheet.tsx
+++ components/ListingsBottomSheet.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
 import { Listing } from "@/types";
 import { useMemo, useRef } from "react";
 import BottomSheetFlatL from "@/components/BottomSheetFlatL";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import Fonts from "@/constants/Fonts";
 
 interface Props {
   listings: Listing[];
@@ -14,24 +17,64 @@ const ListingsBottomSheet = ({ listings, category }: Props) => {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ["10%", "100%"], []);
 
+  const onPressShowMap = () => {
+    bottomSheetRef.current?.collapse();
+  };
+
   return (
-    <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
+    <BottomSheet
+      index={1}
+      ref={bottomSheetRef}
+      snapPoints={snapPoints}
+      // enablePanDownToClose={true}
+      handleIndicatorStyle={{ backgroundColor: Colors.gray }}
+      style={styles.sheetContainer}
+    >
       <View style={{ flex: 1 }}>
         <BottomSheetFlatL category={category} listings={listings} />
+        <View style={styles.mapBtn}>
+          <TouchableOpacity onPress={onPressShowMap} style={styles.btn}>
+            <Text style={{ color: "white", fontFamily: Fonts.fontSemiBold }}>
+              Map
+            </Text>
+            <Ionicons name={"map"} size={20} color={"white"} />
+          </TouchableOpacity>
+        </View>
       </View>
     </BottomSheet>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: "grey",
+  sheetContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
   },
   contentContainer: {
     flex: 1,
     alignItems: "center",
+  },
+  mapBtn: {
+    position: "absolute",
+    width: "100%",
+    bottom: 30,
+    alignItems: "center",
+  },
+  btn: {
+    backgroundColor: Colors.dark,
+    padding: 16,
+    height: 50,
+    flexDirection: "row",
+    gap: 6,
+    borderRadius: 30,
   },
 });
 

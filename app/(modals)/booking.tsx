dd8aa626aac1e1862d64c59e966347fd4b852c0f
Herby <herbyosbourne31@gmail.com>
2024-06-06 13:25:38 -0500

    added blurView to modal

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native';
+import {View, Text, StyleSheet} from "react-native";
+import {BlurView} from "expo-blur";
+import {SafeAreaView, useSafeAreaInsets} from "react-native-safe-area-context";
 
 const Page = () => {
+  const insets = useSafeAreaInsets();
+
+  // console.log(data);
   return (
-    <View>
+    // <SafeAreaView style={{flex: 1}}>
+    <BlurView
+      intensity={70}
+      style={[styles.container]}
+      experimentalBlurMethod={"dimezisBlurView"}>
       <Text>Booking</Text>
-    </View>
+    </BlurView>
+    // </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 100,
+  },
+  blurContainer: {
+    flex: 1,
+    padding: 20,
+    margin: 16,
+    textAlign: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    borderRadius: 20,
+  },
+});
 
 export default Page;
